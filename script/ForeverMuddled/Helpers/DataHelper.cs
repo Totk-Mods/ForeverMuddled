d6609fd7fd83e6ce6267a28f136da6f075df7f7c
ArchLeaders <archleadership28@gmail.com>
2024-08-09 09:30:39 -0700

    Create DataHelper

---
+++ script/ForeverMuddled/Helpers/DataHelper.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.HighPerformance.Buffers;
+using Revrs.Buffers;
+using TotkCommon;
+
+namespace ForeverMuddled.Helpers;
+
+static class DataHelper
+{
+    public static ArraySegmentOwner<byte> ReadAndDecompress(string path, out int dictioanryId)
+    {
+        using FileStream fs = File.OpenRead(path);
+        int size = Convert.ToInt32(fs.Length);
+        using SpanOwner<byte> raw = SpanOwner<byte>.Allocate(size);
+        fs.Read(raw.Span);
+        ArraySegmentOwner<byte> decompressed = ArraySegmentOwner<byte>.Allocate(Zstd.GetDecompressedSize(raw.Span));
+        Totk.Zstd.Decompress(raw.Span, decompressed.Segment, out dictioanryId);
+        return decompressed;
+    }
+}
