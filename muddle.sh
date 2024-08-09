474764133e353ef030c222ebff26f6a5fd8f0d76
ArchLeaders <archleadership28@gmail.com>
2024-08-08 21:58:01 -0700

    Update bash script

--- muddle.sh
+++ muddle.sh
@@ -1,6 +1,7 @@
-printf "Muddling enemies..."
+printf "Muddling enemies...\n"
 
 dotnet run --project ./script/ForeverMuddled/ForeverMuddled.csproj
 
-printf "Enemies muddled!"
+printf "Enemies muddled!\n"
 read -n 1 -s -r -p "Press any key to continue . . . "
+printf "\n"
