de34555bab297ba8b5021a3c8d70e0cd12d49902
ArchLeaders <archleadership28@gmail.com>
2024-08-09 10:27:35 -0700

    Parse muddle factor

---
+++ script/ForeverMuddled/Helpers/InputHelper.cs
@@ -0,0 +1,26 @@
+﻿using ForeverMuddled.Models;
+using System.Globalization;
+
+namespace ForeverMuddled.Helpers;
+
+static class InputHelper
+{
+    public static float GetMuddleFactor(string arg, out MuddleMode mode)
+    {
+        if (string.IsNullOrEmpty(arg)) {
+            mode = MuddleMode.Set;
+            return float.PositiveInfinity;
+        }
+
+        mode = arg[0] switch {
+            '+' => MuddleMode.Add,
+            '*' => MuddleMode.Multiply,
+            _ => MuddleMode.Set,
+        };
+
+        return float.Parse(mode switch {
+            MuddleMode.Multiply or MuddleMode.Add => arg[1..],
+            _ => arg
+        }, CultureInfo.InvariantCulture);
+    }
+}
--- script/ForeverMuddled/Program.cs
+++ script/ForeverMuddled/Program.cs
@@ -3,11 +3,24 @@ using ForeverMuddled.Models;
 
 string outputDirectory = "output";
 
+float muddleFactor = float.PositiveInfinity;
+MuddleMode mode = MuddleMode.Set;
+
+if (args.Length > 0) {
+    muddleFactor = InputHelper.GetMuddleFactor(args[0], out mode);
+}
+
+Console.WriteLine($"Muddling with a factor of {mode switch {
+    MuddleMode.Add => "+",
+    MuddleMode.Multiply => "*",
+    _ => ""
+}}{muddleFactor}");
+
 IEnumerable<Actor> actors = ActorCollector.CollectVanillaActors();
 actors = actors.Concat(ActorCollector.CollectCustomActors());
 
 foreach (Actor actor in actors) {
-    if (actor.Muddle(1.0f)) {
+    if (actor.Muddle(muddleFactor, mode)) {
         actor.Save(outputDirectory);
     }
 }
