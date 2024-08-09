726edfdc46ad046191188fe0acb9ac2e4f43ca0e
ArchLeaders <archleadership28@gmail.com>
2024-08-09 09:31:52 -0700

    Stub Actor class

---
+++ script/ForeverMuddled/Models/Actor.cs
@@ -0,0 +1,27 @@
+﻿using BymlLibrary;
+using BymlLibrary.Nodes.Containers;
+using ForeverMuddled.Helpers;
+using Revrs.Buffers;
+using SarcLibrary;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ForeverMuddled.Models;
+
+class Actor
+{
+    private const string ACTOR_PARAM_NAME = "Actor/{0}.engine__actor__ActorParam.bgyml";
+
+    private readonly string _filePath;
+    private readonly string _actorName;
+    private readonly Sarc _actorPack;
+    private readonly int _dictionaryId;
+
+    public Actor(string filePath)
+    {
+        _filePath = filePath;
+        _actorName = Path.GetFileNameWithoutExtension(filePath.AsSpan()[..^3]).ToString();
+
+        using ArraySegmentOwner<byte> actorPackData = DataHelper.ReadAndDecompress(filePath, out _dictionaryId);
+        _actorPack = Sarc.FromBinary(actorPackData.Segment);
+    }
+}
