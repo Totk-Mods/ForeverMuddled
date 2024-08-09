d6e975e41fdb047ee0f9aaea9bbae68d734cda83
ArchLeaders <archleadership28@gmail.com>
2024-08-09 09:35:22 -0700

    Create ActorCollector

---
+++ script/ForeverMuddled/Helpers/ActorCollector.cs
@@ -0,0 +1,37 @@
+﻿using ForeverMuddled.Models;
+using TotkCommon;
+
+namespace ForeverMuddled.Helpers;
+
+static class ActorCollector
+{
+    public static IEnumerable<Actor> CollectVanillaActors()
+    {
+        string actorsDirectory = Path.Combine(Totk.Config.GamePath, "Pack", "Actor");
+
+        if (!Directory.Exists(actorsDirectory)) {
+            throw new Exception(message: $"""
+                The Actors directory '{actorsDirectory}' could not be found.
+                """);
+        }
+
+        return Directory
+            .EnumerateFiles(actorsDirectory,
+                searchPattern: "*.pack.zs", SearchOption.TopDirectoryOnly)
+            .Select(x => new Actor(x));
+    }
+
+    public static IEnumerable<Actor> CollectCustomActors()
+    {
+        string customActorsDirectory = "custom";
+
+        if (!Directory.Exists(customActorsDirectory)) {
+            return [];
+        }
+
+        return Directory
+            .EnumerateFiles(customActorsDirectory,
+                searchPattern: "*.pack*", SearchOption.TopDirectoryOnly)
+            .Select(x => new Actor(x));
+    }
+}
